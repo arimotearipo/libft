b42e18e66edb5fa36ae6c4c5af2e85388d3827bc
Wan Nor Adzahari <wnabwt@yahoo.com>
2022-05-25 18:22:30 +0800

    Edited ft_substrnoquote

--- ft_substrnoquote.c
+++ ft_substrnoquote.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substrnoquote.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwan-taj <wwan-taj@student.42kl.edu.m      +#+  +:+       +#+        */
+/*   By: wwan-taj <wwan-taj@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:38:28 by wwan-taj          #+#    #+#             */
-/*   Updated: 2022/05/25 14:38:52 by wwan-taj         ###   ########.fr       */
+/*   Updated: 2022/05/25 18:22:00 by wwan-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ char    *ft_substrnoquote(char const *s, unsigned int start, size_t len)
     char    *str;
     size_t    i;
 
-    if (!s)
-        return (ft_strdup(""));
-    if (ft_strlen(s) < start)
+    if (!s || ft_strlen(s) < start)
         return (ft_strdup(""));
     if (len > ft_strlen(s) - start)
         len = ft_strlen(s) - start;
@@ -32,9 +30,13 @@ char    *ft_substrnoquote(char const *s, unsigned int start, size_t len)
         str[i] = s[start];
         i++;
         start++;
-        if ((s[start] == '\'' || s[start] == '"')
-            && (s[start + 1] == '\'' || s[start + 1] == '"'))
-            start += 2;
+        if ((s[start] == '\'' || s[start] == '"') && (s[start + 1] > 32))
+        {
+            if ((s[start + 1] == '\'' || s[start + 1] == '"'))
+                start += 2;
+            else
+                start += 1;
+        }
     }
     str[i] = '\0';
     return (str);
