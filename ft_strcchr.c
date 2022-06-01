9c3395729f8bf1950e368ce06a50e30ec47d402d
Wan nor adzahari Wan tajuddin <wwan-taj@u91z03s02.42kl.edu.my>
2022-06-01 21:39:04 +0800

    fixed ft_strcchr

--- ft_strcchr.c
+++ ft_strcchr.c
@@ -6,7 +6,7 @@
 /*   By: wwan-taj <wwan-taj@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:18:54 by wwan-taj          #+#    #+#             */
-/*   Updated: 2022/06/01 18:29:17 by wwan-taj         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:38:33 by wwan-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ int	ft_strcchr(char *s1, char *s2, char c)
 	int		res;
 	char	*str;
 
-	printf("ADASDASDASDASD\n");
 	i = 0;
 	while (s1[i] != '\0' && s1[i] != c)
 		i++;
 	str = ft_substr(s1, 0, i);
-	printf("str in function: %s\n", str);
 	res = ft_strcmp(str, s2);
 	free(str);
 	return (res);
