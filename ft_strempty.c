cd2667c12c6bb680703d0ba696ec8cd5f52ff4fa
Wan nor adzahari Wan tajuddin <wwan-taj@u81z05s06.42kl.edu.my>
2022-06-22 13:47:21 +0800

    Modified function name for ft_strempty

--- ft_strempty.c
+++ ft_strempty.c
@@ -6,7 +6,7 @@
 /*   By: wwan-taj <wwan-taj@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 13:43:54 by wwan-taj          #+#    #+#             */
-/*   Updated: 2022/06/22 13:44:07 by wwan-taj         ###   ########.fr       */
+/*   Updated: 2022/06/22 13:46:33 by wwan-taj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 Will check if the string is empty (full of whitespaces only) or not.
 Return 1 if it is not empty, and 0 if it is empty.
 */
-int	emptystring(char *str)
+int	ft_strempty(char *str)
 {
 	int	i;
 
